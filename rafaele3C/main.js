e37015b68cc976de7d8a76dca91e2828ce162fd8
Anjosrafa <160125985+Anjosrafa@users.noreply.github.com>
2024-03-21 09:10:51 -0300

    Add files via upload

---
+++ rafaele3C/main.js
@@ -0,0 +1,15 @@
+
+const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+for()
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;)
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    console.log();
+}
